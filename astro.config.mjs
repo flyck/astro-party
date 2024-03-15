54f7d92950d3f76d5bbf69efd86d55f39dc1b8e5
flyck <felix.brilej@googlemail.com>
2024-03-15 18:21:51 +0100

    chore: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), tailwind()],
-  output: "server"
+  output: "server",
+  adapter: netlify()
 });
